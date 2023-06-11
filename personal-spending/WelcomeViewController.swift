3bbcdb79b5b256cc318bced7219234be3dff2013
truonghaily211194 <haily.211194@gmail.com>
2023-06-11 21:51:01 +0700

    update define

--- personal-spending/WelcomeViewController.swift
+++ personal-spending/WelcomeViewController.swift
@@ -27,7 +27,7 @@ class WelcomeViewController: UIViewController, MailDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        descriptionLabel.text = ExtenStrings.descriptions.first
+        descriptionLabel.text = ExtenStrings.descriptions.randomElement()
 //        if #available(iOS 15.0, *) {
 //            let appearance = UINavigationBarAppearance()
 //            appearance.configureWithDefaultBackground()
